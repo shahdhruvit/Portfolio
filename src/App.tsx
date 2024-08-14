216b13247dcc401a5961c7729ed79e9da0fbf6aa
shahdhruvit <shahdhruvit2702@gmail.com>
2024-08-14 09:55:55 +0530

    Added react-pdf cdn

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,8 @@ import { createTheme, MantineProvider } from '@mantine/core';
 import { pdfjs } from 'react-pdf';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs';
 
 function App() {
   useEffect(()=>{
