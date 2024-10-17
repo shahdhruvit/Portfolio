bcc09154f38c15ea1563213d5855a6c3b8a456b0
shahdhruvit <shahdhruvit2702@gmail.com>
2024-10-17 22:46:50 +0530

    commit

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -8,7 +8,7 @@ export function Loader() {
   return (
     <div className="relative flex h-full w-full animate-[ping_1.5s_ease-in-out_1_4.5s]  items-center justify-center px-20 pb-20 pt-8 ">
       <IconCloud iconSlugs={Slugs} />
-      <IconHexagon className="absolute -z-10 animate-[spin_5s_linear_infinite]" size={120} color="#64FFDA" stroke={1.25}/>
+      <IconHexagon className="absolute -z-10 animate-[spin_3s_linear_infinite]" size={120} color="#64FFDA" stroke={1.25}/>
       <div className="text-6xl font-mono text-primaryColor font-semibold absolute -z-10">DS</div>
     </div>
   );
